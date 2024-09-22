d6c86fe2f07b05884ca2de6f5dc857e4a11afdfc
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-09-23 00:15:38 +0200

    Changed update file

--- update_git.sh
+++ update_git.sh
@@ -5,5 +5,12 @@ git config --global --add safe.directory /var/www/flagtrainer >> /var/log/flagtr
 git config --global --list >> /var/log/flagtrainer_update.log 2>&1
 echo "Running update script..." >> /var/log/flagtrainer_update.log
 whoami >> /var/log/flagtrainer_update.log
+
 cd /var/www/flagtrainer
+
+# Discard all local changes and forcefully take the latest from GitHub
+git reset --hard HEAD >> /var/log/flagtrainer_update.log 2>&1
+git clean -fd >> /var/log/flagtrainer_update.log 2>&1
+
+# Pull the latest changes from GitHub
 git pull origin main >> /var/log/flagtrainer_update.log 2>&1
